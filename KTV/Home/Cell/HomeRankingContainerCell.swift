06b0379ebf37b116c5050d5bfced24ad0472239a
momogoyo <metheduxk@gmail.com>
2025-09-04 13:43:45 +0900

    feat: HomeRankingContainerCell, HomeRankingItemCell 추가

---
+++ KTV/Home/Cell/HomeRankingContainerCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeRankingContainerCell.swift
+//  KTV
+//
+//  Created by 현유진 on 9/4/25.
+//
+
+import UIKit
+
+class HomeRankingContainerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ KTV/Home/Cell/HomeRankingItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeRankingItemCell.swift
+//  KTV
+//
+//  Created by 현유진 on 9/4/25.
+//
+
+import UIKit
+
+class HomeRankingItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
